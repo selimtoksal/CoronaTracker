6e25f06ee5d326e5529ee97514dc799dbf21ac26
necati <necatisozer@gmail.com>
2020-04-02 16:56:34 +0300

    Update ImageView extension in Kotlin way

--- app/src/main/java/co/icanteach/app/coronatracker/core/Extensions.kt
+++ app/src/main/java/co/icanteach/app/coronatracker/core/Extensions.kt
@@ -2,8 +2,8 @@ package co.icanteach.app.coronatracker.core
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.annotation.LayoutRes
-import androidx.appcompat.widget.AppCompatImageView
 import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
@@ -12,7 +12,10 @@ import com.bumptech.glide.Glide
 /**
  * http://kotlinextensions.com/
  */
-fun <T : ViewDataBinding> ViewGroup?.inflate(@LayoutRes layoutId: Int, attachToParent: Boolean = true): T {
+fun <T : ViewDataBinding> ViewGroup?.inflate(
+    @LayoutRes layoutId: Int,
+    attachToParent: Boolean = true
+): T {
     return DataBindingUtil.inflate(
         LayoutInflater.from(this!!.context),
         layoutId,
@@ -22,6 +25,6 @@ fun <T : ViewDataBinding> ViewGroup?.inflate(@LayoutRes layoutId: Int, attachToP
 }
 
 @BindingAdapter("imageUrl")
-fun setImageUrl(imageView: AppCompatImageView, url: String?) {
-    Glide.with(imageView.context).load(url).into(imageView)
+fun ImageView.setImageUrl(url: String?) {
+    Glide.with(context).load(url).into(this)
 }
