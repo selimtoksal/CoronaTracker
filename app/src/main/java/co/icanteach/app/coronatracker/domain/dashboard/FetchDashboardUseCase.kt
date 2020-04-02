f08e525053fd601ef1ce5eb4dce8d69a744dd2a6
necati <necatisozer@gmail.com>
2020-04-02 15:49:57 +0300

    Apply default dispatcher for mapping operations

--- app/src/main/java/co/icanteach/app/coronatracker/domain/dashboard/FetchDashboardUseCase.kt
+++ app/src/main/java/co/icanteach/app/coronatracker/domain/dashboard/FetchDashboardUseCase.kt
@@ -1,19 +1,23 @@
 package co.icanteach.app.coronatracker.domain.dashboard
 
 import co.icanteach.app.coronatracker.core.Resource
+import co.icanteach.app.coronatracker.core.inject.DefaultDispatcher
 import co.icanteach.app.coronatracker.data.CoronaTrackerRepository
 import co.icanteach.app.coronatracker.domain.DashboardItemMapper
 import co.icanteach.app.coronatracker.domain.dashboard.model.DashboardItem
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class FetchDashboardUseCase @Inject constructor(
     private val repository: CoronaTrackerRepository,
-    private val mapper: DashboardItemMapper
+    private val mapper: DashboardItemMapper,
+    @DefaultDispatcher private val dispatcher: CoroutineDispatcher
 ) {
-    @ExperimentalCoroutinesApi
+    @OptIn(ExperimentalCoroutinesApi::class)
     fun fetchDashboard(): Flow<List<DashboardItem>> {
         val totalDataFlow = repository.fetchTotalData()
         val countriesDataFlow = repository.fetchCountriesData()
@@ -26,6 +30,6 @@ class FetchDashboardUseCase @Inject constructor(
                     // todo update for error and loading.
                     else -> emptyList()
                 }
-            }
+            }.flowOn(dispatcher)
     }
 }
--- app/src/main/java/co/icanteach/app/coronatracker/domain/news/FetchNewsUseCase.kt
+++ app/src/main/java/co/icanteach/app/coronatracker/domain/news/FetchNewsUseCase.kt
@@ -1,17 +1,23 @@
 package co.icanteach.app.coronatracker.domain.news
 
 import co.icanteach.app.coronatracker.core.Resource
+import co.icanteach.app.coronatracker.core.inject.DefaultDispatcher
 import co.icanteach.app.coronatracker.data.CoronaTrackerRepository
 import co.icanteach.app.coronatracker.domain.news.model.News
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class FetchNewsUseCase @Inject constructor(
     private val repository: CoronaTrackerRepository,
-    private val mapper: CoronaNewsMapper
+    private val mapper: CoronaNewsMapper,
+    @DefaultDispatcher private val dispatcher: CoroutineDispatcher
 ) {
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     fun fetchCoronaNews(): Flow<Resource<List<News>>> {
         return repository
             .fetchCoronaNews()
@@ -19,6 +25,6 @@ class FetchNewsUseCase @Inject constructor(
                 resource.map {
                     mapper.mapFromResponse(it.result)
                 }
-            }
+            }.flowOn(dispatcher)
     }
 }
